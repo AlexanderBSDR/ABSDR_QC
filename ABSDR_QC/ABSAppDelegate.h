adaa31043c0bf0fb2975036d02511619a30543c4
CIG account <cig@CIGs-MacBook.local>
2014-01-31 15:37:57 +0200

    Initial Commit

---
+++ ABSDR_QC/ABSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ABSAppDelegate.h
+//  ABSDR_QC
+//
+//  Created by CIG account on 1/31/14.
+//  Copyright (c) 2014 CIG account. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ABSDR_QC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ABSDR_QC
+//
+//  Created by CIG account on 1/31/14.
+//  Copyright (c) 2014 CIG account. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ABSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ABSAppDelegate class]));
+    }
+}
