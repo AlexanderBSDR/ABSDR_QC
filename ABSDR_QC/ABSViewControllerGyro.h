904d3b3a4e41259db39b779a9b253e293cbd8dce
AlexanderBSDR <kpikpi_121@i.ua>
2014-02-09 00:02:01 +0200

    Evening

---
+++ ABSDR_QC/ABSViewControllerGyro.h
@@ -0,0 +1,13 @@
+//
+//  ABSViewControllerGyro.h
+//  ABSDR_QC
+//
+//  Created by Alexander Bilyi on 2/8/14.
+//  Copyright (c) 2014 CIG account. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABSViewControllerGyro : UIViewController
+
+@end
---
+++ ABSDR_QC/ABSViewControllerGyro.m
@@ -0,0 +1,52 @@
+//
+//  ABSViewControllerGyro.m
+//  ABSDR_QC
+//
+//  Created by Alexander Bilyi on 2/8/14.
+//  Copyright (c) 2014 CIG account. All rights reserved.
+//
+
+#import "ABSViewControllerGyro.h"
+
+@interface ABSViewControllerGyro ()
+@property (weak, nonatomic) IBOutlet UILabel *accX;
+@property (weak, nonatomic) IBOutlet UILabel *accY;
+@property (weak, nonatomic) IBOutlet UILabel *accZ;
+@property (weak, nonatomic) IBOutlet UILabel *maxAccX;
+@property (weak, nonatomic) IBOutlet UILabel *maxAccY;
+@property (weak, nonatomic) IBOutlet UILabel *maxAccZ;
+@property (weak, nonatomic) IBOutlet UILabel *rotX;
+@property (weak, nonatomic) IBOutlet UILabel *rotY;
+@property (weak, nonatomic) IBOutlet UILabel *rotZ;
+@property (weak, nonatomic) IBOutlet UILabel *maxRotX;
+@property (weak, nonatomic) IBOutlet UILabel *maxRotY;
+@property (weak, nonatomic) IBOutlet UILabel *maxRotZ;
+
+@end
+
+@implementation ABSViewControllerGyro
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)resetMaxValue:(id)sender {
+}
+
+@end
